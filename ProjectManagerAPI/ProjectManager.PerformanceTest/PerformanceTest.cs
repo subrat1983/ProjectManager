8e073f40d087b439d5657c08f9875e20dbc6e8cd
subrat1983 <53242208+subrat1983@users.noreply.github.com>
2019-09-04 21:03:10 -0500

    PerformanceTest Added

---
+++ ProjectManagerAPI/ProjectManager.PerformanceTest/PerformanceTest.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using NBench;
+using ProjectManagerAPI.Controllers;
+
+
+namespace ProjectManagerAPI.PerformanceTest
+{
+    [TestClass]
+    public class PerformanceTest
+    {
+        [PerfBenchmark(NumberOfIterations = 1, RunMode = RunMode.Throughput,
+       TestMode = TestMode.Test, SkipWarmups = true)]
+        [ElapsedTimeAssertion(MaxTimeMilliseconds = 5000)]
+        public void PerformanceTests()
+        {
+            // Set up Prerequisites   
+            var controllerObject = new ProjectController();
+            // Act on Test  
+            var response = controllerObject.RetrieveProjects();
+            // Assert the result  
+            Assert.IsTrue(response != null);
+        }
+    }
+}
---
+++ ProjectManagerAPI/ProjectManager.PerformanceTest/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("ProjectManager.PerformanceTest")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("HP Inc.")]
+[assembly: AssemblyProduct("ProjectManager.PerformanceTest")]
+[assembly: AssemblyCopyright("Copyright © HP Inc. 2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("8232e729-aea1-40b0-a627-fde52f291151")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
